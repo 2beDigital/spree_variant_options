e25d94f3c0fa76dbdc2dc0539f3511677691e31e
Noel <noel@2bedigital.com>
2015-11-16 11:00:55 +0000

    added alert on select variants option and improvements on variants script

---
+++ app/overrides/spree/add_alert_select_option.rb
@@ -0,0 +1,14 @@
+Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
+                     :name => "add_alert_select_option",
+                     :insert_bottom => "body",
+                     :text => "<div id='shadow-alert'></div>
+                                <div id='alert-select-variant'>
+                                  <p><i class='fa fa-exclamation-triangle fa-2x'></i></p>
+                                  <p><%= t(:select_an_option) %></p>
+                                  <%= button_tag :class => 'btn', :id => 'alert-select-variant-button', :type => :submit do %>
+                                      <%= t(:accept) %>
+                                  <% end %>
+                                </div>",
+                     :disabled => false)
+
+
