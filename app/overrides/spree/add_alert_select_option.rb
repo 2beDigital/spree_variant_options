aea6b09dc2985b9b06d54bcfee8fb6594c64c9a1
Noel <noel@2bedigital.com>
2015-11-27 12:34:19 +0000

    bug on js and modified button alert

--- app/overrides/spree/add_alert_select_option.rb
+++ app/overrides/spree/add_alert_select_option.rb
@@ -5,7 +5,7 @@ Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                                 <div id='alert-select-variant'>
                                   <p><i class='fa fa-exclamation-triangle fa-2x'></i></p>
                                   <p><%= t(:select_an_option) %></p>
-                                  <%= button_tag :class => 'btn', :id => 'alert-select-variant-button', :type => :submit do %>
+                                  <%= button_tag :class => 'btn btn-primary', :id => 'alert-select-variant-button', :type => :submit do %>
                                       <%= t(:accept) %>
                                   <% end %>
                                 </div>",
