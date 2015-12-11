d4b41f037df24ee9e6354167be1534feded71e7d
Noel <noel@2bedigital.com>
2015-12-11 07:43:56 +0000

    fix on alert_select_option

--- app/overrides/spree/add_alert_select_option.rb
+++ app/overrides/spree/add_alert_select_option.rb
@@ -1,14 +1,15 @@
-Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
-                     :name => "add_alert_select_option",
-                     :insert_bottom => "body",
-                     :text => "<div id='shadow-alert'></div>
-                                <div id='alert-select-variant'>
-                                  <p><i class='fa fa-exclamation-triangle fa-2x'></i></p>
-                                  <p><%= t(:select_an_option) %></p>
-                                  <%= button_tag :class => 'btn btn-primary', :id => 'alert-select-variant-button', :type => :submit do %>
-                                      <%= t(:accept) %>
-                                  <% end %>
-                                </div>",
+Deface::Override.new(:virtual_path => "spree/products/show",
+                     :name => "add_alert_select_option_to_show",
+                     :insert_before => "erb[silent]:contains('cache')",
+                     :text => "<% content_for :head do %>
+                                  <div id='shadow-alert'></div>
+                                   <div id='alert-select-variant'>
+                                    <p><i class='fa fa-exclamation-triangle fa-2x'></i></p>
+                                    <p><%= t(:select_an_option) %></p>
+                                    <%= button_tag :class => 'btn', :id => 'alert-select-variant-button', :type => :submit do %>
+                                        <%= t(:accept) %>
+                                    <% end %>
+                                  </div>
+                               <% end %>",
                      :disabled => false)
-
 
